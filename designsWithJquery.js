8929991e9cfb8fb2b989b29b75d70a3585251d2d
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2018-01-14 15:19:01 +0100

    create design with jQuery

---
+++ designsWithJquery.js
@@ -0,0 +1,50 @@
+//Define constants
+const canvas = $("#pixel_canvas");
+const colorPicker = $("#colorPicker");
+const sizePicker = $('#sizePicker');
+const eraser = $('img');
+//Define variables
+var color = colorPicker.val();
+
+//Add event listeners
+addEventListeners();
+
+//Define makeGrid function
+function makeGrid() {
+  let height, width, grid;
+  height = $('#input_height').val();
+  width = $("#input_width").val();
+  //Clear previous table
+  canvas.html("");
+
+  //Create the table
+  for (var rows = 0; rows < height; rows++) {
+    grid += ("<tr>")
+    for (var columns = 0; columns < width; columns++) {
+      grid += ("<td></td>")
+    }
+    grid += ("</tr>")
+  }
+  canvas.html(grid);
+};
+
+//Gather all event listeners in one method
+function addEventListeners() {
+  //Create grid with given height and width
+  sizePicker.submit(function(e) {
+    e.preventDefault();
+    makeGrid();
+  });
+  //Change color when new color is selected on the colorPicker
+  colorPicker.change(function () {
+    color = colorPicker.val();
+  });
+  //Switch color to white when eraser image is clicked
+  eraser.on('click' , function () {
+    color = '#ffffff';
+  });
+  //Change background color of cell when clicked
+  canvas.on('click', 'td', function() {
+    $(this).css('background', color);
+  });
+}
